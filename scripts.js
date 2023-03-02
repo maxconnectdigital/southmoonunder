401a51065e4a2e7de3256c3b646a2d7e2ffebac2
Zack Warren <zachary.warren@me.com>
2023-03-02 14:20:56 -0700

    Initial stuff

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-console.log('here from jsdelivr');
+$(document).ready(function() {
+  StoreFront.cartReady(function() {
+    var storeSelector = document.querySelector('span#cphContent_StoreSelector1_CtaMessage')
+
+    if (storeSelector) {
+
+      if (storeSelector.innerText.startsWith('Your current')) {
+
+
+        var segments = storeSelector.innerText.split('. ')
+
+        var currentStore = segments[0]
+          .replace('Your current pickup location is', '')
+          .trim()
+          .toLowerCase()
+
+        var textString = 'Your selected store: <span style="font-weight: bold; text-transform: capitalize">' + currentStore + '</span> <span style="text-decoration: underline; margin-left: 7px;">Change Store</span>'
+
+        storeSelector.innerHTML = textString
+      } else {
+        storeSelector.innerHTML = 'Select a location to see Pick Up In Store availability. <span style="text-decoration:underline;font-weight:bold;margin-left:7px">Select Store</span>'
+      }
+
+    }
+  })
+})
